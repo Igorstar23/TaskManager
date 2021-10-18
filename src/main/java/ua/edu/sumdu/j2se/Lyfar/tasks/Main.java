e38a683833fef48827078de8d3c7307450e3c195
Igorstar23 <larstar@ukr.net>
2021-10-18 19:31:15 +0300

    Edit main

--- src/main/java/ua/edu/sumdu/j2se/Lyfar/tasks/Main.java
+++ src/main/java/ua/edu/sumdu/j2se/Lyfar/tasks/Main.java
@@ -2,7 +2,7 @@ package ua.edu.sumdu.j2se.Lyfar.tasks;
 
 public class Main {
 
-	public static void main(String[] args) {
-		System.out.println("Hello World");
-	}
+	   public static void main(String[] args) {
+		      System.out.println("Hello");
+	   }
 }
